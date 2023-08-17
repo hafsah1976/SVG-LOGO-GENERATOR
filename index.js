1febfdeb2718e22c84320f52eac4711f67f52dd8
Khan-HB <hafsah999@gmail.com>
2023-08-17 17:49:11 -0400

    adding testing suite

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-// const shapes = require("./lib/shapes");
 const { lowercaseColours } = require("./lib/colours");
-// const hexcolours = require("./lib/hexcolours");
-const { Triangle, Square, Circle,SquarewithBR } = require("./lib/shapes");
+const { Triangle, Square, Circle, SquarewithBR } = require("./lib/shapes");
 
 function userInput() {
   inquirer
@@ -13,62 +11,64 @@ function userInput() {
         name: "text",
         message:
           "Please Enter 3 Characters of the logo you would like to create:",
-        validate: (input) => 
-        {
-          if(input.length == 3){
-            return true;}
-            else if(input.length < 3){
-            console.log("\n Please enter at least 3 characters.");}
-          else {
-        return console.log("\n You cannot enter more than 3 characters.");
+        validate: (input) => {
+          if (input.length == 3) {
+            return true;
+          } else if (input.length < 3) {
+            console.log("\n Please enter at least 3 characters.");
+          } else {
+            return console.log("\n You cannot enter more than 3 characters.");
           }
-        }
+        },
       },
       {
         type: "list",
         name: "textColorChoice",
-        message:"Please choose a color format:",
-        choices:['Color Names', 'Hexadecimal Codes']
+        message: "Please choose a color format:",
+        choices: ["Color Names", "Hexadecimal Codes"],
       },
       {
         type: "input",
-        name:"textColor",
-        message:"Please enter a color name: ",
-        when:(answer)=> {
-          if(answer.textColorChoice==='Color Names'){
+        name: "textColor",
+        message: "Please enter a color name: ",
+        when: (answer) => {
+          if (answer.textColorChoice === "Color Names") {
             return true;
           }
           return false;
         },
-        validate:(answer)=>{
-          let input=answer.toLowerCase();
-          for(var i = 0, arrayLength = lowercaseColours.length;i <arrayLength;i++){
-            if(input.indexOf(lowercaseColours[i])!=-1){
+        validate: (answer) => {
+          let input = answer.toLowerCase();
+          for (
+            var i = 0, arrayLength = lowercaseColours.length;
+            i < arrayLength;
+            i++
+          ) {
+            if (input.indexOf(lowercaseColours[i]) != -1) {
               return true;
             }
           }
-            return console.log("Please enter a valid color name.");
-          }
-      
+          return console.log("Please enter a valid color name.");
+        },
       },
       {
         type: "input",
-        name:"textColor",
-        message:"Please enter a hexadecimal code for your TEXT, such as #000000: ",
-        when:(answer)=>{
-          if(answer.textColorChoice === 'Hexadecimal Codes') {
+        name: "textColor",
+        message:
+          "Please enter a hexadecimal code for your TEXT, such as #000000: ",
+        when: (answer) => {
+          if (answer.textColorChoice === "Hexadecimal Codes") {
             return true;
-        }
-        return false;
-    },
-    validate: (answer) => {
-      let isValidHexColor=answer;
-      if (isValidHexColor = /^#[0-9A-F]{6}$/i.test(answer)){
-        return true;        
-      }
-    return console.log("\n Please enter a valid hexadecimal code.")
-        }
-
+          }
+          return false;
+        },
+        validate: (answer) => {
+          let isValidHexColor = answer;
+          if ((isValidHexColor = /^#[0-9A-F]{6}$/i.test(answer))) {
+            return true;
+          }
+          return console.log("\n Please enter a valid hexadecimal code.");
+        },
       },
       {
         type: "list",
@@ -81,59 +81,64 @@ function userInput() {
       {
         type: "list",
         name: "shapeColorFormat",
-        message: "Please choose a color format for the SHAPE you have selected:",
-        choices: ['Color Names','Hexadecimal Codes'], 
+        message:
+          "Please choose a color format for the SHAPE you have selected:",
+        choices: ["Color Names", "Hexadecimal Codes"],
       },
       //validate shapecolorformat
       {
-        type:'input',
-        name :'shapeColor',
-        message :"Enter the color name to fill in the SHAPE.",
-        when :(answer)=>{
-          if(answer.shapeColorFormat==='Color Names'){
+        type: "input",
+        name: "shapeColor",
+        message: "Enter the color name to fill in the SHAPE.",
+        when: (answer) => {
+          if (answer.shapeColorFormat === "Color Names") {
             return true;
           }
           return false;
-      },
+        },
         validate: (answer) => {
-         let input=answer.toLowerCase();
-         for(var i =0, arrayLength=lowercaseColours.length;i<arrayLength;i++){
-          if (input.indexOf(lowercaseColours[i] !=-1)){
+          let input = answer.toLowerCase();
+          for (
+            var i = 0, arrayLength = lowercaseColours.length;
+            i < arrayLength;
+            i++
+          ) {
+            if (input.indexOf(lowercaseColours[i] != -1)) {
+              return true;
+            }
+          }
+          return "Please enter a valid color name";
+        },
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message:
+          "Please enter a hexadecimal code for your SHAPE, such as #000000: ",
+        when: (input) => {
+          if (input.shapeColorFormat === "Hexadecimal Codes") {
             return true;
           }
-        }
-        return "Please enter a valid color name";
-          }
+          return false;
         },
-    {
-      type:'input',
-      name:'shapeColor',
-      message:"Please enter a hexadecimal code for your SHAPE, such as #000000: ",
-    when:(input)=>{
-      if(input.shapeColorFormat === 'Hexadecimal Codes'){
-        return true;
-              }
-              return false;
-    },
-    validate: (input) => {
-      let isValidHexColor=input;
-      if (isValidHexColor = /^#[0-9A-F]{6}$/i.test(input)){
-        return true;        
-      }
-    return console.log("\n Please enter a valid hexadecimal code.")
-    },  
-  },
+        validate: (input) => {
+          let isValidHexColor = input;
+          if ((isValidHexColor = /^#[0-9A-F]{6}$/i.test(input))) {
+            return true;
+          }
+          return console.log("\n Please enter a valid hexadecimal code.");
+        },
+      },
     ])
     .then((answer) => {
-        writeToFile("logo.svg", answer);
-      }
-    );
+      writeToFile("logo.svg", answer);
+    });
 }
 
 function writeToFile(fileName, answer) {
   // Taking from user
   let svgData = ""; //from the user input, data is accumalated in this variable as a string
-  let shapeInput = ""; 
+  let shapeInput = "";
 
   svgData = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
   svgData += "<g>";
@@ -154,7 +159,7 @@ function writeToFile(fileName, answer) {
       break;
     case "Round cornered Square":
       shapeInput = new SquarewithBR();
-      svgData += `<rect x="60" y="10" rx="10" ry="10" fill="${answer.shapeColor}"/>`;
+      svgData += `<rect x="70" y="37" rx="30" ry="30" width="160" height="160" fill="${answer.shapeColor}"/>`;
       break;
     default:
       // Handle unexpected shape input
